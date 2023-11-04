b418128d28d0ccc9d8e53b0a359f1049f830a3d3
Ben Juntilla <46634820+benjuntilla@users.noreply.github.com>
2023-11-04 15:36:14 -0700

    improve navbar appearance

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -19,50 +19,19 @@ export default function RootLayout({
     <html lang="en" className="dark">
       <body className={inter.className}>
         <div className="navbar bg-bg">
-          <div className="flex-none">
-            <button className="btn btn-square btn-ghost">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block w-5 h-5 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                ></path>
-              </svg>
-            </button>
-          </div>
           <div className="flex-1">
             <a href="/">
               <Image
                 src="/CodeCupid-.png"
                 width={100}
                 height={100}
-                alt="Picture of the author"
+                alt="Code Cupid icon"
               />
             </a>
-            <a className="btn btn-ghost normal-case text-xl">Code Cupid</a>
+            <h1 className="text-lg text-primary-content">Code Cupid</h1>
           </div>
           <div className="flex-none">
-            <button className="btn btn-square btn-ghost">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block w-5 h-5 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-                ></path>
-              </svg>
-            </button>
+            <button className="btn btn-primary">Log in</button>
           </div>
         </div>
         <div className="flex justify-center">{children}</div>
