b21efd112b77ebdb073c56b63382ffa77e45d1f4
Ben Juntilla <46634820+benjuntilla@users.noreply.github.com>
2023-11-04 14:19:31 -0700

    ditto

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body className={inter.className}>
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-bg">
           <div className="flex-none">
             <button className="btn btn-square btn-ghost">
               <svg
