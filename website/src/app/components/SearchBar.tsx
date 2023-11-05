6e534c7db3d91f90b7c34316ee0018fde1176e0c
Ben Juntilla <46634820+benjuntilla@users.noreply.github.com>
2023-11-05 01:15:48 -0700

    add searchbar

---
+++ website/src/app/components/SearchBar.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { searchProjects } from "@/lib/api/project";
+import { useState } from "react";
+import { useUserContext } from "./UserProvider";
+
+export default function SearchBar() {
+  const context = useUserContext();
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchClick = async () => {
+    try {
+      const skills = "";
+      const page_size = 10;
+      const page_number = 1;
+      const results = await searchProjects(
+        context.client,
+        searchTerm,
+        skills,
+        page_size,
+        page_number,
+      );
+      console.log(results);
+    } catch (error) {
+      console.error("Error searching projects:", error);
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search..."
+        className="input input-bordered w-full max-w-xs"
+        onChange={(e) => {
+          e.preventDefault();
+          setSearchTerm(e.target.value);
+        }}
+      />
+      <button onClick={handleSearchClick}>Search</button>
+    </div>
+  );
+}
--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Image from "next/image";
 import LoginButton from "./components/LoginButton";
 import { CgProfile } from "react-icons/cg";
-import { initializeFirebase } from "@/lib/firebase";
-import { createApolloClient } from "@/lib/apollo";
 import { UserProvider } from "./components/UserProvider";
-
-const inter = Inter({ subsets: ["latin"] });
-
+import SearchBar from "./components/SearchBar";
+import React from "react";
+export const useClient = true;
 export const metadata: Metadata = {
   title: "Code Cupid",
   description: "Match with coders who love what you love",
@@ -20,36 +17,59 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const app = initializeFirebase();
-  const client = createApolloClient();
   return (
-    <html lang="en" className="dark">
-      <body className={inter.className}>
-        <div className="navbar bg-primary">
-          <div className="flex-1">
-            <a href="/">
-              <Image
-                src="/CodeCupid-.png"
-                width={100}
-                height={100}
-                alt="Code Cupid icon"
-              />
-            </a>
-            <a href="/">
-              <h1 className="text-lg">Code Cupid</h1>
-            </a>
+    <html lang="en">
+      <UserProvider>
+        <body>
+          <div className="navbar bg-primary">
+            <div className="flex-1">
+              <a href="/">
+                <Image
+                  src="/CodeCupid-.png"
+                  width={100}
+                  height={100}
+                  alt="Code Cupid icon"
+                />
+              </a>
+              <a href="/">
+                <h1>Code Cupid</h1>
+              </a>
+
+              <div className="navbar-start"></div>
+
+              <SearchBar />
+              <div className="navbar-end">
+                <button className="btn btn-ghost btn-circle">
+                  <div className="indicator">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                      />
+                    </svg>
+                    <span className="badge badge-xs badge-primary indicator-item"></span>
+                  </div>
+                </button>
+              </div>
+            </div>
+            <div className="flex-none">
+              <LoginButton />
+              <a href="/profile">
+                <CgProfile className="mx-3" size="2em" />
+              </a>
+            </div>
           </div>
-          <div className="flex-none">
-            <LoginButton />
-            <a href="/profile">
-              <CgProfile className="mx-3" size="2em" />
-            </a>
-          </div>
-        </div>
-        <UserProvider>
           <div className="flex justify-center">{children}</div>
-        </UserProvider>
-      </body>
+        </body>
+      </UserProvider>
     </html>
   );
 }
