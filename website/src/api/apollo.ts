c29eff06102df13e4d056cbe233aef90a171a3ef
densumesh <dens.sumesh79@gmail.com>
2023-11-04 18:47:53 -0700

    remove env vars

--- website/src/api/apollo.ts
+++ website/src/api/apollo.ts
@@ -3,7 +3,7 @@ import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 export const createUserApolloClient = (user_id: string) => {
   return new ApolloClient({
     link: new HttpLink({
-      uri: process.env.VITE_HASURA_DB_URL,
+      uri: "https://quality-oyster-64.hasura.app/v1/graphql",
       headers: {
         "x-hasura-user-role": "user",
         "x-hasura-user-id": user_id,
