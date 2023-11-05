f00906502a35668bd05cf7017ec400a5c00b20de
densumesh <dens.sumesh79@gmail.com>
2023-11-04 18:33:58 -0700

    remove uncessary client

--- website/src/api/apollo.ts
+++ website/src/api/apollo.ts
@@ -1,17 +1,5 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 
-export const createAdminApolloClient = () => {
-  return new ApolloClient({
-    link: new HttpLink({
-      uri: process.env.VITE_HASURA_DB_URL,
-      headers: {
-        "x-hasura-admin-secret": process.env.VITE_HASURA_CLIENT_SECRET!,
-      },
-    }),
-    cache: new InMemoryCache(),
-  });
-};
-
 export const createUserApolloClient = (user_id: string) => {
   return new ApolloClient({
     link: new HttpLink({
