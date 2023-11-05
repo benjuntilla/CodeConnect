8d15c13821a28f2840edee15f64dfe704d01b4fa
Ben Juntilla <46634820+benjuntilla@users.noreply.github.com>
2023-11-05 04:32:43 -0700

    improve navbar

--- website/src/app/components/LoginButton.tsx
+++ website/src/app/components/LoginButton.tsx
@@ -12,7 +12,7 @@ import { getUser } from "@/lib/api/user";
 import { useState, useEffect } from "react";
 import { useUserContext } from "./UserProvider";
 import { getAuth } from "firebase/auth";
-import { set } from "firebase/database";
+import { CgProfile } from "react-icons/cg";
 
 export default function LoginButton() {
   const context = useUserContext();
@@ -42,6 +42,28 @@ export default function LoginButton() {
           >
             Log out
           </button>
+          <button className="btn btn-ghost btn-circle">
+            <div className="indicator">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                />
+              </svg>
+              <span className="badge badge-xs badge-primary indicator-item"></span>
+            </div>
+          </button>
+          <a href="/profile">
+            <CgProfile className="mx-3" size="2em" />
+          </a>
         </>
       ) : (
         <button
@@ -51,7 +73,7 @@ export default function LoginButton() {
               console.log(getUserUID(context.app) ?? "undefined");
               getUser(
                 apolloClient,
-                getUserUID(context.app) ?? "undefined"
+                getUserUID(context.app) ?? "undefined",
               ).then((res) => {
                 if (res.data?.length > 0) {
                   console.log("User doesn't exist; onboarding...");
