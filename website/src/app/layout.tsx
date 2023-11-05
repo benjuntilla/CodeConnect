2decb4786621c43ba89cf05a9e8b11ed9ccae5f1
Ben Juntilla <46634820+benjuntilla@users.noreply.github.com>
2023-11-05 08:21:32 -0700

    switch from `a` to `Link`

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import AuthTray from "./components/AuthTray";
 import { UserProvider } from "./components/UserProvider";
 import React from "react";
+import Link from 'next/link'
+
 export const useClient = true;
 export const metadata: Metadata = {
   title: "Code Cupid",
@@ -21,17 +23,17 @@ export default function RootLayout({
         <body>
           <div className="navbar bg-primary">
             <div className="flex-1">
-              <a href="/">
+              <Link href="/">
                 <Image
                   src="/CodeCupid-.png"
                   width={100}
                   height={100}
                   alt="Code Cupid icon"
                 />
-              </a>
-              <a href="/">
+              </Link>
+              <Link href="/">
                 <p className="text-2xl">Code Cupid</p>
-              </a>
+              </Link>
               {/* <div className="navbar-start"></div>
               <SearchBar />
               <div className="navbar-end"></div> */}
