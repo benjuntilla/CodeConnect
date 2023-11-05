a82cc709920c9e82c695c6f1f2cf043bbea2cbad
Ben Juntilla <46634820+benjuntilla@users.noreply.github.com>
2023-11-05 03:13:09 -0700

    improve search bar

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -38,10 +38,6 @@ export default function RootLayout({
               <div className="navbar-start"></div>
 
               <SearchBar />
-                <div><button className="btn btn-square">
-                      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-              </button>
-              </div>
 
               <div className="navbar-end">
                 <button className="btn btn-ghost btn-circle">
